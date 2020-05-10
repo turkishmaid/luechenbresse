93473a1448f45416ab743e20bd7f3f5b7d365e6c
Sara Ziner <sara.ziner@googlemail.com>
2020-05-10 18:20:03 +0200

    Cleaning setup.py

--- setup.py
+++ setup.py
@@ -7,25 +7,30 @@ WhatIsThisHere
 Created: 03.05.20
 """
 
-from setuptools import setup
+import setuptools
 
-setup(name='luechenbresse',
-      version = '0.1',
-      description = 'Scraping Germany',
-      # url='http://github.com/storborg/funniest',
-      # author='Flying Circus',
-      # author_email='flyingcircus@example.com',
-      license = 'MIT',
-      packages = ['luechenbresse'],
-      include_package_data = True,
-      zip_safe = False,
-#      test_suite = "luechenbresse.test",              # TODO: use tox
-#      test_suite = "tests",
-      scripts = ["bin/zdf-heute", "bin/luechenbresse"],
-      install_requires = [
-            'requests',
-            'feedparser',
-            'bs4',
-            'docopt',
-      ],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='luechenbresse',
+    version = '0.0.1',
+    description = 'Scraping Germany',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/turkishmaid/luechenbresse',
+    author = 'Sara Ziner',
+    # author_email = 'flyingcircus@example.com',
+    license = 'MIT',
+    packages = setuptools.find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    scripts = [ "bin/luechenbresse" ],
+    install_requires = [ 'requests', 'feedparser', 'bs4', 'docopt', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
