73217124a2158bcd21811ab939d8706076053084
Sara Ziner <sara.ziner@googlemail.com>
2020-05-12 01:21:48 +0200

    v0.0.2 mit logging

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='luechenbresse',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Scraping Germany',
     long_description = long_description,
     long_description_content_type = "text/markdown",
