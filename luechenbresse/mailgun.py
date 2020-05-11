da5579f63179ab6da0ee8b7ceef5d8df5c5abf59
Sara Ziner <sara.ziner@googlemail.com>
2020-05-11 23:37:06 +0200

    adding basic mailgun tooling

---
+++ luechenbresse/mailgun.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Convenience API to Mailgun
+
+Created: 11.05.20
+"""
+
+import requests
+from luechenbresse import ini
+
+
+# courtesy https://gist.github.com/lalzada/3938daf1470a3b7ed7d167976a329638
+class Mailgun(object):
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance') or not cls.instance:
+            cls.instance = super().__new__(cls)
+            mailgun = "mailgun"
+            cls.instance.url = ini.get(mailgun, "url")
+            cls.instance.auth_key = ini.get(mailgun, "auth-key")
+            cls.instance.mail_from = ini.get(mailgun, "from")
+            cls.instance.mail_to = ini.get(mailgun, "to")
+            cls.instance.active = cls.instance.url and cls.instance.auth_key and cls.instance.mail_from and cls.instance.mail_to
+        return cls.instance
+
+    def shoot(self, subject, body):
+        if not self.active:
+            print("no mailgun account configured")
+        else:
+            print("sending mail:", subject)
+            try:
+                r = requests.post(
+                    self.url,
+                    auth=("api", self.auth_key),
+                    data={
+                        "from": self.mail_from,
+                        "to": self.mail_to,
+                        "subject": subject,
+                        "text": body
+                    })
+                print("HTTP", r.status_code)
+            except Exception as ex:
+                print(ex.__class__.__name__, str(ex))
+
+if __name__ == "__main__":
+    pass
